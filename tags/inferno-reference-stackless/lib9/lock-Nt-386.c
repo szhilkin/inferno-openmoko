aaa9db44086aae03fe4d0bef84787512e0bc69a4
4239512 <4239512@664bf904-8573-11dd-88bd-cff828a63736>
2008-12-29 22:07:35 +0000

    reference stackless branch

---
+++ tags/inferno-reference-stackless/lib9/lock-Nt-386.c
@@ -0,0 +1,18 @@
+#include "lib9.h"
+
+int
+_tas(int *la)
+{
+	int v;
+#ifdef _MSC_VER
+	_asm {
+		mov eax, la
+		mov ebx, 1
+		xchg	ebx, [eax]
+		mov	v, ebx
+	}
+#else
+	__asm__ ("xchgl %0,%1" : "=r" (v), "=m" (*la) : "0"(1));
+#endif
+	return v;
+}
